14add66daa026e801bc27345a5c1f34d80c0c3de
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-10-10 02:35:37 -0700

    Test6

---
+++ Fahrenheit/Program5.cs
@@ -0,0 +1,8 @@
+﻿// Calculator for Fahrenheit
+double F, C;
+Console.WriteLine("Enter C: ");
+C = Convert.ToDouble(Console.ReadLine());
+F = ( C* 9/5) + 32;
+Console.WriteLine("Result is: " + F);
+
+
